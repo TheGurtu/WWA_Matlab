a96cf470d7e6c9c20a07805287f551ecca671cc6
Stefan Benox <stefan.benox@student.uni-luebeck.de>
2021-02-06 12:52:08 +0100

    funktion leicht veraendert und zufällige Phase mit eingebaut

--- createWaveform.m
+++ createWaveform.m
@@ -1,7 +1,7 @@
 %% Aufgabe 1
 function [sinusoid] = createWaveform(frequency,fs,duration,A)
-n = (1:duration)
-[sinusoid] = A*cos(2*pi*fs*duration*1/frequency*n)
-sound((sinusoid))
+n = 1:(duration*fs)
+[sinusoid] = A*cos(2*pi*frequency*n*(1/fs)*((2*pi).*rand(1)))
+%sound((sinusoid))
 
 end
