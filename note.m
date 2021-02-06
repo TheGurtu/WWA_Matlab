b79b0839761d02a5e4996a62d03234fb8e247348
Stefan Benox <stefan.benox@student.uni-luebeck.de>
2021-02-06 14:48:59 +0100

    schreibfehler in funktion behoben hatte fullDration statt fullDuration geschrieben

--- note.m
+++ note.m
@@ -1,4 +1,4 @@
 %funktion generiert Ton abhaenig von der Tastennummer keynum
 function [tone] = note(keynum,relDuration,fullDuration,fs)
-    tone = createWaveform((440*(2^(1/12)))^(keynum - 49),fs,fullDration*relDuration,1)
+    tone = createWaveform(440*(2^(1/12))^(keynum - 49),fs,relDuration*fullDuration,1)
 end
